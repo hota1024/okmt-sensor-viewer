f1bc5ac8af922bfb33807e5c72eaf6e5cd143f13
= <hotalog@hota1024.com>
2022-01-02 21:59:44 +0900

    :tada: inti

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - okmt-sensor-viewer</title>
+      </Head>
+
+      <div>okmt-sensor-viewer</div>
+    </>
+  )
+}
+
+export default HomePage
